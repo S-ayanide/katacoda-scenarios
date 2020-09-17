9e78918738e0f5a092a72312b14c762843f3464b
Sayan Mondal <sayan.mondal@mayadata.io>
2020-09-17 14:37:20 +0530

    Updated timer.sh
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- getting-started-litmus/timer.sh
+++ getting-started-litmus/timer.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
+
 echo "Welcome to Litmus, Please wait for the resources to load up.."
-x=5
-while [ $x -gt 0 ]do
-sleep 1s
-clear
-echo "Let's get you started"
-x=$(( $x
+sleep 5s
+echo "Let's get you started with Litmus"
