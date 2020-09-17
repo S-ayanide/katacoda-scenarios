ae61b40e6fb08af3c9f3418a4d6af39890887c50
Sayan Mondal <sayan.mondal@mayadata.io>
2020-09-17 14:41:21 +0530

    Updating sleep
    Signed-off-by: Sayan Mondal <sayan.mondal@mayadata.io>

--- getting-started-litmus/timer.sh
+++ getting-started-litmus/timer.sh
@@ -1,3 +1,2 @@
-echo "Welcome to Litmus, Please wait for the resources to load up.."
-sleep 5s
+read -p "Welcome to Litmus, Please wait for the resources to load up.." -t 5
 echo "Let's get you started with Litmus"
