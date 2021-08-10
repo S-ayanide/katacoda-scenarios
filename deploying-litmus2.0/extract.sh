55089fad1b127e154363a3eeff61d3c885822fc7
Sayan Mondal <sayan@chaosnative.com>
2021-08-10 12:00:23 +0530

    Fixing index.json
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- deploying-litmus2.0/extract.sh
+++ deploying-litmus2.0/extract.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+clear
 IP="$(minikube ip)"
 FrontendServiceNodePort="$(kubectl describe svc litmusportal-frontend-service -n litmus | grep NodePort: | grep -oE "[[:digit:]]{1,}")"
 ChaosCenter="$IP:$FrontendServiceNodePort"
+clear
 echo "ChaosCenter is available at $ChaosCenter"
