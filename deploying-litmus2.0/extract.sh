fb6912f3c49bff9c6c6c23fa6ae55174a58d3605
Sayan Mondal <sayan@chaosnative.com>
2021-08-10 11:31:25 +0530

    Fixing index.json
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- deploying-litmus2.0/extract.sh
+++ deploying-litmus2.0/extract.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-while getopts "n:" arg; do
-  case $arg in
-    n) Namespace=$OPTARG;;
-  esac
-done
-
 IP="$(minikube ip)"
-FrontendServiceNodePort="$(kubectl describe svc litmusportal-frontend-service -n $Namespace | grep NodePort: | grep -oE "[[:digit:]]{1,}")"
+FrontendServiceNodePort="$(kubectl describe svc litmusportal-frontend-service -n litmus | grep NodePort: | grep -oE "[[:digit:]]{1,}")"
 export ChaosCenter="$IP:$FrontendServiceNodePort"
