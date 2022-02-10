cfdc21cdcf46f181456f524d634ba26b60797260
Sayan Mondal <sayan@chaosnative.com>
2022-02-10 14:49:52 +0530

    Trying loop over kubectl
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- deploy-litmus2.0-wiht-podtato-head/deploy.sh
+++ deploy-litmus2.0-wiht-podtato-head/deploy.sh
@@ -1,4 +1,9 @@
-kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/2.5.0/mkdocs/docs/2.5.0/litmus-2.5.0.yaml
-kubectl apply -f https://raw.githubusercontent.com/litmuschaos/test-tools/master/custom/workflow-helper/app-deployer/app-manifest/resilient-podtato-head.yaml
-clear
-echo "Litmus 2.0 and Podtato Head application is successfully deployed in this cluster"
+#!/bin/bash
+
+until kubectl config view &> /dev/null
+do
+    echo "Waiting for kubectl host ..."
+    sleep 1
+done
+
+echo -e "\nThe kubectl host is running."
