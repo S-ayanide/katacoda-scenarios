2be999b72e30f29586183e156a7eec79742f45f8
Sayan Mondal <sayan@chaosnative.com>
2022-02-10 17:29:14 +0530

    Updating bash scrip
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- deploy-litmus2.0-wiht-podtato-head/deploy.sh
+++ deploy-litmus2.0-wiht-podtato-head/deploy.sh
@@ -2,7 +2,10 @@
 
 until kubectl config view | grep -m 1 "http"; do echo "Waiting for Kubernetes to setup" && sleep 10 ; done
 
+sleep 5
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/2.5.0/mkdocs/docs/2.5.0/litmus-2.5.0.yaml
+sleep 5
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/test-tools/master/custom/workflow-helper/app-deployer/app-manifest/resilient-podtato-head.yaml
+sleep 5
 clear
 echo "Installing Litmus 2.0 and Podtato Head"
