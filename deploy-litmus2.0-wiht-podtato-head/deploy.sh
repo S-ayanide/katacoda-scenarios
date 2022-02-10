cfd5cdb8ac597643b0033dcd70e137f937b8b304
Sayan Mondal <sayan@chaosnative.com>
2022-02-10 18:25:42 +0530

    Updating script
    Signed-off-by: Sayan Mondal <sayan@chaosnative.com>

--- deploy-litmus2.0-wiht-podtato-head/deploy.sh
+++ deploy-litmus2.0-wiht-podtato-head/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+echo "Please wait for a minute while we install all the necessary services"
 
-until kubectl config view | grep -m 1 "http"; do echo "Waiting for Kubernetes to setup" && sleep 10 ; done
+until kubectl config view | grep -m 1 "http"; do echo "Waiting for Kubernetes to setup" && sleep 20 ; done
 
 sleep 5
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/2.5.0/mkdocs/docs/2.5.0/litmus-2.5.0.yaml
@@ -9,6 +10,6 @@ kubectl apply -f https://raw.githubusercontent.com/litmuschaos/test-tools/master
 sleep 5
 clear
 echo "Installing Litmus 2.0 and Podtato Head"
-sleep 5
+sleep 10
 FrontendServiceNodePort="$(kubectl describe svc litmusportal-frontend-service -n litmus | grep NodePort: | grep -oE "[[:digit:]]{1,}")"
 echo "ChaosCenter is available at PORT $FrontendServiceNodePort"
